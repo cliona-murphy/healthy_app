4ea0497a7acffa5bc813f996a9395058e20b14f9
Cliona Murphy <c.murphy99@nuigalway.ie>
2020-12-28 13:09:05 +0000

    wrapping children in singleChildScroll to remove error - not working for sign-in

--- healthy_app/lib/screens/authentication/register.dart
+++ healthy_app/lib/screens/authentication/register.dart
@@ -43,55 +43,57 @@ class _RegisterState extends State<Register> {
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                decoration: textInputDecoration.copyWith(hintText: "email"),
-                validator: (val) => val.isEmpty ? "Enter an email" : null,
-                  onChanged: (val) {
-                    setState(() => email = val);
-                  }
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                  decoration: textInputDecoration.copyWith(hintText: "password"),
-                  validator: (val) => val.length < 6 ? "Enter a password of 6 characters or more" : null,
-                  obscureText: true,
-                  onChanged: (val) {
-                    setState(() => password = val);
-                  }
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                  color: Colors.pink[400],
-                  child: Text(
-                    "Register",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  onPressed: () async {
-                    if (_formKey.currentState.validate()){
-                      setState(() => loading = true);
-                      dynamic result = await _auth.registerWithEmail(email, password);
-                      if (result == null) {
-                        setState(() {
-                          error  = "please supply valid email";
-                          loading = false;
-                        });
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 20.0),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: "email"),
+                  validator: (val) => val.isEmpty ? "Enter an email" : null,
+                    onChanged: (val) {
+                      setState(() => email = val);
+                    }
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                    decoration: textInputDecoration.copyWith(hintText: "password"),
+                    validator: (val) => val.length < 6 ? "Enter a password of 6 characters or more" : null,
+                    obscureText: true,
+                    onChanged: (val) {
+                      setState(() => password = val);
+                    }
+                ),
+                SizedBox(height: 20.0),
+                RaisedButton(
+                    color: Colors.pink[400],
+                    child: Text(
+                      "Register",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () async {
+                      if (_formKey.currentState.validate()){
+                        setState(() => loading = true);
+                        dynamic result = await _auth.registerWithEmail(email, password);
+                        if (result == null) {
+                          setState(() {
+                            error  = "please supply valid email";
+                            loading = false;
+                          });
+                        }
                       }
                     }
-                  }
-              ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(
-                  color: Colors.red,
                 ),
-              )
-            ],
+                SizedBox(height: 12.0),
+                Text(
+                  error,
+                  style: TextStyle(
+                    color: Colors.red,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- healthy_app/lib/screens/authentication/sign_in.dart
+++ healthy_app/lib/screens/authentication/sign_in.dart
@@ -40,57 +40,58 @@ class _SignInState extends State<SignIn> {
           ),
         ],
       ),
-      body: Container(
+      body: SingleChildScrollView( child: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                  decoration: textInputDecoration.copyWith(hintText: "email"),
-                  validator: (val) => val.isEmpty ? "Enter an email" : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                }
-              ),
-              SizedBox(height: 20.0),
-              TextFormField(
-                  decoration: textInputDecoration.copyWith(hintText: "password"),
-                obscureText: true,
-                  validator: (val) => val.length < 6 ? "Enter a password of 6 characters or more" : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                }
-              ),
-              SizedBox(height: 20.0),
-              RaisedButton(
-                color: Colors.pink[400],
-                child: Text(
-                  "Sign in",
-                  style: TextStyle(color: Colors.white),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 20.0),
+                TextFormField(
+                    decoration: textInputDecoration.copyWith(hintText: "email"),
+                    validator: (val) => val.isEmpty ? "Enter an email" : null,
+                  onChanged: (val) {
+                    setState(() => email = val);
+                  }
                 ),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()){
-                    setState(() => loading = true);
-                    dynamic result = await _auth.signInWithEmail(email, password);
-                    if (result == null) {
-                      setState(() {
-                      error = "could not sign in with those credentials";
-                      loading = false;
-                      });
+                SizedBox(height: 20.0),
+                TextFormField(
+                    decoration: textInputDecoration.copyWith(hintText: "password"),
+                  obscureText: true,
+                    validator: (val) => val.length < 6 ? "Enter a password of 6 characters or more" : null,
+                  onChanged: (val) {
+                    setState(() => password = val);
+                  }
+                ),
+                SizedBox(height: 20.0),
+                RaisedButton(
+                  color: Colors.pink[400],
+                  child: Text(
+                    "Sign in",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState.validate()){
+                      setState(() => loading = true);
+                      dynamic result = await _auth.signInWithEmail(email, password);
+                      if (result == null) {
+                        setState(() {
+                        error = "could not sign in with those credentials";
+                        loading = false;
+                        });
+                      }
                     }
                   }
-                }
-              ),
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(
-                  color: Colors.red,
                 ),
-              )
-            ],
+                SizedBox(height: 12.0),
+                Text(
+                  error,
+                  style: TextStyle(
+                    color: Colors.red,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
