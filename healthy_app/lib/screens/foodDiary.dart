dee0ab424c588e440ce4bae60d7aa5da62afa9fe
Cliona Murphy <c.murphy99@nuigalway.ie>
2021-02-03 15:41:35 +0000

    creating all pages required for app and navigating between them using navbar

---
+++ healthy_app/lib/screens/foodDiary.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:healthy_app/services/auth.dart';
+import 'package:healthy_app/services/database.dart';
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:healthy_app/screens/home/userSettings_list.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+
+class FoodDiary extends StatelessWidget {
+
+  final AuthService _auth = AuthService();
+
+  Widget build(BuildContext context){
+    return StreamProvider<QuerySnapshot>.value(
+      value: DatabaseService().userSettings,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title : Text("Food Diary"),
+          backgroundColor: Colors.grey,
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text("logout"),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            ),
+          ],
+        ),
+        body: Text("This is the food diary"),
+      ),
+    );
+  }
+}
