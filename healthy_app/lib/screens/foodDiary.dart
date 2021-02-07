8b7e63d773ceb40a25123d17fdae800b20a4c0b4
Cliona Murphy <c.murphy99@nuigalway.ie>
2021-02-07 16:42:38 +0000

    changing breakfast box to be clickable and dialog box pops up with fields for name and calories

--- healthy_app/lib/screens/foodDiary.dart
+++ healthy_app/lib/screens/foodDiary.dart
@@ -15,6 +15,48 @@ class FoodDiary extends StatefulWidget {
 class _FoodDiaryState extends State<FoodDiary> {
   final AuthService _auth = AuthService();
 
+  TextEditingController customController = TextEditingController();
+
+  Future <String> onContainerTapped(BuildContext context){
+    print("Here");
+    return showDialog(context: context, builder: (context) {
+      return AlertDialog(
+          title: Text("What did you eat?"),
+          content: Container(
+            height: 100,
+            child : SingleChildScrollView(
+              child: Column(
+                children: [
+                  //Text("Food name"),
+                  TextField(
+                    //controller: customController,
+                    decoration: InputDecoration(
+                      hintText: "food name",
+                    ),
+                  ),
+                  TextField(
+                    //controller: customController,
+                    decoration: InputDecoration(
+                      hintText: "calories",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        actions: <Widget> [
+          MaterialButton(
+              elevation: 5.0,
+              child: Text("Submit"),
+              onPressed: () {
+                Navigator.of(context).pop(customController.text.toString());
+              },
+          ),
+        ],
+      );
+   });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
@@ -26,76 +68,90 @@ class _FoodDiaryState extends State<FoodDiary> {
               child: new Column(
                   children: [
                     Padding(padding: EdgeInsets.only(top: 0.0)),
-                    Text('Breakfast',
-                      style: new TextStyle(
-                          color: Colors.blue, fontSize: 20.0),),
-                    Padding(padding: EdgeInsets.only(top: 10.0)),
-                    TextField(
-                      decoration: InputDecoration(
-                          border: new OutlineInputBorder(
-                            borderRadius: new BorderRadius.circular(25.0),
-                            borderSide: new BorderSide(),
+                    GestureDetector(
+                      onTap: () {
+                        print("here - Text on tap");
+                        },
+                        child: Text('Breakfast',
+                        style: new TextStyle(
+                        color: Colors.blue, fontSize: 20.0),),
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 10.0)),
+                        Container(
+                          width: 300,
+                          height: 60,
+                          child: InkWell(
+                            onTap: () {
+                              onContainerTapped(context).then((onValue){
+                                Text breakfastText = Text("$onValue");
+                               // SnackBar customSnackBar = SnackBar(content: Text("hi $onValue"));
+                              });
+                            },
+                              child: Text('Enter what you ate for breakfast')),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blueAccent)
                           ),
-                          hintText: 'What did you eat for breakfast?'
-                      ),
-                    ),
-                    Padding(padding: EdgeInsets.only(top: 20.0)),
-                    Text('Lunch',
-                      style: new TextStyle(
-                          color: Colors.blue, fontSize: 20.0),),
-                    Padding(padding: EdgeInsets.only(top: 10.0)),
-                    TextField(
-                      decoration: InputDecoration(
-                          border: new OutlineInputBorder(
-                            borderRadius: new BorderRadius.circular(25.0),
-                            borderSide: new BorderSide(),
+                               //get food entered for breakfast from db and display here
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 20.0)),
+                        Text('Lunch',
+                          style: new TextStyle(
+                              color: Colors.blue, fontSize: 20.0),),
+                        Padding(padding: EdgeInsets.only(top: 10.0)),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: new OutlineInputBorder(
+                                borderRadius: new BorderRadius.circular(25.0),
+                                borderSide: new BorderSide(),
+                              ),
+                              hintText: 'What did you eat for lunch?'
                           ),
-                          hintText: 'What did you eat for lunch?'
-                      ),
-                    ),
-                    Padding(padding: EdgeInsets.only(top: 20.0)),
-                    Text('Dinner',
-                      style: new TextStyle(
-                          color: Colors.blue, fontSize: 20.0),),
-                    Padding(padding: EdgeInsets.only(top: 10.0)),
-                    TextField(
-                      decoration: InputDecoration(
-                          border: new OutlineInputBorder(
-                            borderRadius: new BorderRadius.circular(25.0),
-                            borderSide: new BorderSide(),
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 20.0)),
+                        Text('Dinner',
+                          style: new TextStyle(
+                              color: Colors.blue, fontSize: 20.0),),
+                        Padding(padding: EdgeInsets.only(top: 10.0)),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: new OutlineInputBorder(
+                                borderRadius: new BorderRadius.circular(25.0),
+                                borderSide: new BorderSide(),
+                              ),
+                              hintText: 'What did you eat for dinner?'
                           ),
-                          hintText: 'What did you eat for dinner?'
-                      ),
-                    ),
-                    Padding(padding: EdgeInsets.only(top: 20.0)),
-                    Text('Snacks',
-                      style: new TextStyle(
-                          color: Colors.blue, fontSize: 20.0),),
-                    Padding(padding: EdgeInsets.only(top: 10.0)),
-                    TextField(
-                      decoration: InputDecoration(
-                          border: new OutlineInputBorder(
-                            borderRadius: new BorderRadius.circular(25.0),
-                            borderSide: new BorderSide(),
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 20.0)),
+                        Text('Snacks',
+                          style: new TextStyle(
+                              color: Colors.blue, fontSize: 20.0),),
+                        Padding(padding: EdgeInsets.only(top: 10.0)),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: new OutlineInputBorder(
+                                borderRadius: new BorderRadius.circular(25.0),
+                                borderSide: new BorderSide(),
+                              ),
+                              hintText: 'What snacks did you have?'
                           ),
-                          hintText: 'What snacks did you have?'
-                      ),
-                    ),
-                    Padding(padding: EdgeInsets.only(top: 20.0)),
-                    Text('Water',
-                      style: new TextStyle(
-                          color: Colors.blue, fontSize: 20.0),),
-                    Padding(padding: EdgeInsets.only(top: 10.0)),
-                    TextField(
-                      decoration: InputDecoration(
-                          border: new OutlineInputBorder(
-                            borderRadius: new BorderRadius.circular(25.0),
-                            borderSide: new BorderSide(),
+                        ),
+                        Padding(padding: EdgeInsets.only(top: 20.0)),
+                        Text('Water',
+                          style: new TextStyle(
+                              color: Colors.blue, fontSize: 20.0),),
+                        Padding(padding: EdgeInsets.only(top: 10.0)),
+                        GestureDetector(
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: new OutlineInputBorder(
+                                  borderRadius: new BorderRadius.circular(25.0),
+                                  borderSide: new BorderSide(),
+                                ),
+                                fillColor: Colors.blue, //not working for some reason
+                                hintText: 'How much water did you drink?'
+                            ),
                           ),
-                          fillColor: Colors.blue, //not working for some reason
-                          hintText: 'How much water did you drink?'
-                      ),
-                    ),
+                    )
                   ]),
           ),
         ),
