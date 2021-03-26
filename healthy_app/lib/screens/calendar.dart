27a8250c0d371e9fad932af32fdf4c3666ba1426
Cliona Murphy <c.murphy99@nuigalway.ie>
2021-03-26 12:21:00 +0000

    getting calendary working

--- healthy_app/lib/screens/calendar.dart
+++ healthy_app/lib/screens/calendar.dart
@@ -4,6 +4,8 @@ import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart' show C
 import 'package:flutter_calendar_carousel/classes/event.dart';
 import 'package:flutter_calendar_carousel/classes/event_list.dart';
 import 'package:intl/intl.dart' show DateFormat;
+//import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarView extends StatefulWidget {
 
@@ -20,45 +22,17 @@ class _CalendarViewState extends State<CalendarView> {
   String _currentMonth = DateFormat.yMMM().format(DateTime.now());
   DateTime _targetDateTime = DateTime.now();
 
+  CalendarController _controller = CalendarController();
+
+  void initState() {
+    super.initState();
+    //_controller = CalendarController();
+  }
+
   //EventList<Event> _markedDateMap = new EventList<Event>();
 
   @override
   Widget build(BuildContext context) {
-    /// Example with custom icon
-    final _calendarCarousel = CalendarCarousel<Event>(
-      onDayPressed: (date, events) {
-        this.setState(() => _currentDate = date);
-      },
-      weekendTextStyle: TextStyle(
-        color: Colors.red,
-      ),
-      thisMonthDayBorderColor: Colors.grey,
-//          weekDays: null, /// for pass null when you do not want to render weekDays
-      headerText: DateFormat.yMMM().format(_targetDateTime),
-      //markedDatesMap: _markedDateMap,
-      height: 325.0,
-      selectedDateTime: _currentDate2,
-      showIconBehindDayText: true,
-//          daysHaveCircularBorder: false, /// null for not rendering any border, true for circular border, false for rectangular border
-      customGridViewPhysics: NeverScrollableScrollPhysics(),
-      markedDateShowIcon: true,
-      markedDateIconMaxShown: 2,
-      selectedDayTextStyle: TextStyle(
-        color: Colors.yellow,
-      ),
-      todayTextStyle: TextStyle(
-        color: Colors.blue,
-      ),
-      minSelectedDate: _currentDate.subtract(Duration(days: 360)),
-      maxSelectedDate: _currentDate.add(Duration(days: 360)),
-      todayButtonColor: Colors.transparent,
-      todayBorderColor: Colors.green,
-      markedDateMoreShowTotal:
-      true, // null for not showing hidden events indicator
-//          markedDateIconMargin: 9,
-//          markedDateIconOffset: 3,
-    );
-
     return new Scaffold(
         appBar: new AppBar(
           title: new Text("Calendar"),
@@ -68,64 +42,66 @@ class _CalendarViewState extends State<CalendarView> {
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              //custom icon
               Container(
-                margin: EdgeInsets.symmetric(horizontal: 16.0),
-                child: _calendarCarousel,
-              ), // This trailing comma makes auto-formatting nicer for build methods.
-              //custom icon without header
-              Container(
-                margin: EdgeInsets.only(
-                  top: 30.0,
-                  bottom: 16.0,
-                  left: 16.0,
-                  right: 16.0,
+                child: TableCalendar(
+                  calendarController: _controller,
+                  availableCalendarFormats: const {
+                    CalendarFormat.week: 'Two Weeks',
+                    CalendarFormat.month: 'Week',
+                    CalendarFormat.twoWeeks: "Month",
+                  },
                 ),
-                child: new Row(
-                  children: <Widget>[
-                    FlatButton(
-                      child: Text('PREV'),
-                      onPressed: () {
-                        setState(() {
-                          _targetDateTime = DateTime(
-                              _targetDateTime.year, _targetDateTime.month - 1);
-                          _currentMonth =
-                              DateFormat.yMMM().format(_targetDateTime);
-                        });
-                      },
-                    ),
-                    Padding(padding: EdgeInsets.fromLTRB(25.0, 0.0, 0.0, 0.0)),
-                    Expanded(
-                        child: Text(
-                          _currentMonth,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24.0,
-                          ),
-                        )),
-                    FlatButton(
-                      child: Text('NEXT'),
-                      onPressed: () {
-                        setState(() {
-                          _targetDateTime = DateTime(
-                              _targetDateTime.year, _targetDateTime.month + 1);
-                          _currentMonth =
-                              DateFormat.yMMM().format(_targetDateTime);
-                        });
-                      },
-                    )
-                  ],
-                ),
-              ), //
+                // child: TableCalendar(
+                //   initialCalendarFormat: CalendarFormat.month,
+                //   calendarStyle: CalendarStyle(
+                //       todayColor: Colors.blue,
+                //       selectedColor: Theme.of(context).primaryColor,
+                //       todayStyle: TextStyle(
+                //           fontWeight: FontWeight.bold,
+                //           fontSize: 22.0,
+                //           color: Colors.white),
+                // ),
+                //   headerStyle: HeaderStyle(
+                //     centerHeaderTitle: true,
+                //     formatButtonDecoration: BoxDecoration(
+                //       color: Colors.black,
+                //       borderRadius: BorderRadius.circular(22.0),
+                //     ),
+                //     formatButtonTextStyle: TextStyle(color: Colors.white),
+                //     formatButtonShowsNext: true,
+                //   ),
+                //   startingDayOfWeek: StartingDayOfWeek.monday,
+                //   onDaySelected: (date, events,e) {
+                //     print(date.toUtc());
+                //   },
+                //   builders: CalendarBuilders(
+                //     selectedDayBuilder: (context, date, events) => Container(
+                //         margin: const EdgeInsets.all(5.0),
+                //         alignment: Alignment.center,
+                //         decoration: BoxDecoration(
+                //             color: Theme.of(context).primaryColor,
+                //             borderRadius: BorderRadius.circular(8.0)),
+                //         child: Text(
+                //           date.day.toString(),
+                //           style: TextStyle(color: Colors.white),
+                //         )),
+                //     todayDayBuilder: (context, date, events) => Container(
+                //         margin: const EdgeInsets.all(5.0),
+                //         alignment: Alignment.center,
+                //         decoration: BoxDecoration(
+                //             color: Colors.blue,
+                //             borderRadius: BorderRadius.circular(8.0)),
+                //         child: Text(
+                //           date.day.toString(),
+                //           style: TextStyle(color: Colors.white),
+                //         ),
+                //     ),
+                //   ),
+                //   calendarController: _controller,
+                // ),
+              ),
             ],
           ),
         ));
-  }
-
-  String getCurrentDate(){
-    var date = new DateTime.now().toString();
-    var dateParse = DateTime.parse(date);
-    var formattedDate = "${dateParse.day}/${dateParse.month}/${dateParse.year}";
-    return formattedDate;
   }
 }
