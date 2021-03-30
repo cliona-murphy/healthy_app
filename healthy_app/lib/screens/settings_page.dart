6f14e9bf7092da6de5a9cf4e7b53fa8e69b5e42b
Cliona Murphy <c.murphy99@nuigalway.ie>
2021-03-30 11:11:33 +0100

    building settings page with appropriate fields

--- healthy_app/lib/screens/settings_page.dart
+++ healthy_app/lib/screens/settings_page.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cupertino_setting_control/cupertino_setting_control.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -11,6 +12,10 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   final FirebaseAuth auth = FirebaseAuth.instance;
   String userId = "";
+  bool _chatResult = true;
+  String _searchAreaResult = 'Germany';
+  bool _titleOnTop = false;
+
 
   void initState() {
     super.initState();
@@ -27,23 +32,237 @@ class _SettingsPageState extends State<SettingsPage> {
     return uid;
   }
 
+  void onChatSettingChange(bool data) {
+    setState(() {
+      _chatResult = data;
+    });
+  }
+
+  void onSearchAreaChange(String data) {
+    setState(() {
+      _searchAreaResult = data;
+    });
+    
+  }
+
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Firestore.instance.collection('settings').document(userId).snapshots(),
-        builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-        // var targetIntake = snapshot.data['kcalIntakeTarget'];
-        // var targetOutput = snapshot.data['kcalOutputTarget'];
-        // var targetWater = snapshot.data['waterIntakeTarget'];
-        return new Scaffold(
-          appBar: new AppBar(
-            title: new Text("Settings"),
+    final titleOnTopSwitch = SettingRow(
+        rowData: SettingsYesNoConfig(
+            initialValue: _titleOnTop, title: 'Title on top'),
+        config: const SettingsRowConfiguration(showAsSingleSetting: true),
+        onSettingDataRowChange: (newVal) => setState(() {
+          _titleOnTop = newVal;
+        }));
+
+    final profileSettingsTile = new Material(
+      color: Colors.transparent,
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Padding(
+              padding: EdgeInsets.fromLTRB(25.0, 5.0, 25.0, 5.0),
+              child: const Text(
+                'Profile',
+                style: TextStyle(
+                  color: CupertinoColors.systemBlue,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
+          new SettingRow(
+            rowData: SettingsDropDownConfig(
+                title: 'User Area',
+                initialKey: _searchAreaResult,
+                choices: {
+                  'Ireland': 'Ireland',
+                  'United Kingdom': 'United Kingdom',
+                }),
+            onSettingDataRowChange: onSearchAreaChange,
+            config: SettingsRowConfiguration(
+                showAsTextField: false,
+                showTitleLeft: !_titleOnTop,
+                showTopTitle: _titleOnTop,
+                showAsSingleSetting: false),
           ),
-          body: SingleChildScrollView(
-            child: Container(
-              child: Text("test"),
+          SizedBox(height: _titleOnTop ? 10.0 : 0.0),
+          new SettingRow(
+            rowData: SettingsSliderConfig(
+              title: 'Age',
+              from: 18,
+              to: 120,
+              initialValue: 20,
+              justIntValues: true,
+              unit: ' years',
             ),
-            // child: Expanded(
+            onSettingDataRowChange: (double resultVal) {},
+            config: SettingsRowConfiguration(
+                showAsTextField: false,
+                showTitleLeft: !_titleOnTop,
+                showTopTitle: _titleOnTop,
+                showAsSingleSetting: false),
+          ),
+          SizedBox(height: _titleOnTop ? 10.0 : 0.0),
+          new SettingRow(
+            rowData: SettingsSliderFromToConfig(
+              title: 'Weight',
+              from: 40,
+              to: 120,
+              initialFrom: 50,
+              initialTo: 80,
+              justIntValues: true,
+              unit: 'kg',
+            ),
+            onSettingDataRowChange: (List<double> resultVals) {},
+            config: SettingsRowConfiguration(
+                showAsTextField: false,
+                showTitleLeft: !_titleOnTop,
+                showTopTitle: _titleOnTop,
+                showAsSingleSetting: false),
+          ),
+          SizedBox(height: _titleOnTop ? 10.0 : 0.0),
+        ],
+      ),
+    );
+
+    final accountSettingsTile = new Material(
+      color: Colors.transparent,
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Padding(
+              padding: EdgeInsets.fromLTRB(25.0, 5.0, 25.0, 5.0),
+              child: const Text(
+                'Account',
+                style: TextStyle(
+                  color: CupertinoColors.systemBlue,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
+          SizedBox(height: _titleOnTop ? 10.0 : 0.0),
+          new SettingRow(
+            rowData: SettingsSliderConfig(
+              title: 'Daily Calorie Intake Target',
+              from: 1500,
+              to: 5000,
+              initialValue: 2500,
+              justIntValues: true,
+              unit: ' kcal',
+            ),
+            onSettingDataRowChange: (double resultVal) {},
+            config: SettingsRowConfiguration(
+                showAsTextField: false,
+                showTitleLeft: !_titleOnTop,
+                showTopTitle: _titleOnTop,
+                showAsSingleSetting: false),
+          ),
+          SizedBox(height: _titleOnTop ? 10.0 : 0.0),
+          new SettingRow(
+            rowData: SettingsSliderConfig(
+              title: 'Daily Calorie Output Target',
+              from: 1000,
+              to: 4000,
+              initialValue: 2000,
+              justIntValues: true,
+              unit: ' kcal',
+            ),
+            onSettingDataRowChange: (List<double> resultVals) {},
+            config: SettingsRowConfiguration(
+                showAsTextField: false,
+                showTitleLeft: !_titleOnTop,
+                showTopTitle: _titleOnTop,
+                showAsSingleSetting: false),
+          ),
+          SizedBox(height: _titleOnTop ? 10.0 : 0.0),
+          new SettingRow(
+            rowData: SettingsSliderConfig(
+              title: 'Daily Water Intake Target',
+              from: 2,
+              to: 5,
+              initialValue: 2,
+              justIntValues: true,
+              unit: ' litres',
+            ),
+            onSettingDataRowChange: (List<double> resultVals) {},
+            config: SettingsRowConfiguration(
+                showAsTextField: false,
+                showTitleLeft: !_titleOnTop,
+                showTopTitle: _titleOnTop,
+                showAsSingleSetting: false),
+          ),
+        ],
+      ),
+    );
+
+    final modifyProfileTile = new Material(
+        color: Colors.transparent,
+        child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              const Padding(
+                  padding: EdgeInsets.fromLTRB(25.0, 5.0, 25.0, 5.0),
+                  child: const Text(
+                    'Profile Options',
+                    style: TextStyle(
+                      color: CupertinoColors.systemBlue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15.0,
+                    ),
+                  )),
+              SettingRow(
+                rowData: SettingsButtonConfig(
+                  title: 'Delete Profile',
+                  tick: true,
+                  functionToCall: () {},
+                ),
+                style: const SettingsRowStyle(
+                    backgroundColor: CupertinoColors.lightBackgroundGray),
+                config: SettingsRowConfiguration(
+                    showAsTextField: false,
+                    showTitleLeft: !_titleOnTop,
+                    showTopTitle: _titleOnTop,
+                    showAsSingleSetting: false),
+              )
+            ]));
+
+    final List<Widget> widgetList = [
+      titleOnTopSwitch,
+      const SizedBox(height: 15.0),
+      profileSettingsTile,
+      const SizedBox(height: 15.0),
+      accountSettingsTile,
+      // const SizedBox(height: 15.0),
+      // legalStuff,
+      const SizedBox(height: 15.0),
+      Row(children: [Expanded(child: modifyProfileTile)]),
+    ];
+
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 50, 0, 0),
+          child: ListView(
+              children: widgetList,
+              physics: const AlwaysScrollableScrollPhysics())),
+    );
+    // return StreamBuilder(
+    //   stream: Firestore.instance.collection('settings').document(userId).snapshots(),
+    //     builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+    //     // var targetIntake = snapshot.data['kcalIntakeTarget'];
+    //     // var targetOutput = snapshot.data['kcalOutputTarget'];
+    //     // var targetWater = snapshot.data['waterIntakeTarget'];
+    //     return new Scaffold(
+    //       appBar: new AppBar(
+    //         title: new Text("Settings"),
+    //       ),
+    //       body: SingleChildScrollView(
+    //         child: Container(
+    //           child: Text("test"),
+    //         ),
+    //         // child: Expanded(
             //   child: Flexible(
             //     child: ListView(
             //       padding: const EdgeInsets.all(8),
@@ -67,9 +286,9 @@ class _SettingsPageState extends State<SettingsPage> {
             //     ),
             //   ),
             // ),
-          ),
-        );
-        //return SettingsTile(targetIntake: targetIntake, targetOutput: targetOutput, targetWater: targetWater);
-    });
+    //       ),
+    //     );
+    //     //return SettingsTile(targetIntake: targetIntake, targetOutput: targetOutput, targetWater: targetWater);
+    // });
   }
 }
