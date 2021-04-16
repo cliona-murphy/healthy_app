d530d72e4feabb2d22dab22ba9c036023e0b78a9
Cliona Murphy <c.murphy99@nuigalway.ie>
2021-04-16 15:13:12 +0100

    attempting to add leading 0 to minute string when string contains one digit - needs to be tested

--- healthy_app/lib/services/database.dart
+++ healthy_app/lib/services/database.dart
@@ -590,6 +590,10 @@ class DatabaseService {
   String getCurrentTime() {
     var time = new DateTime.now().toString();
     var timeParse = DateTime.parse(time);
+    String minute = timeParse.minute.toString();
+    if (timeParse.minute < 10){
+      minute = "0${minute}";
+    }
     var formattedTime = "${timeParse.hour}:${timeParse.minute}";
     return formattedTime;
   }
